e7c0635619f93295df2d0712bc6fbd721f792b65
apoorvcodes <tofu.develops@gmail.com>
2022-10-05 23:26:26 +0530

    feat: home ui done

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Stack = createBottomTabNavigator();
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+const Stack = createMaterialBottomTabNavigator();
 import Home from './screens/Home';
 import Video from './screens/VideoStack';
 import Audio from './screens/Audio';
@@ -10,10 +12,26 @@ import Audio from './screens/Audio';
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Audio" component={Audio} />
-        <Stack.Screen name="Video" component={Video} />
+      <Stack.Navigator
+   
+               barStyle={{ backgroundColor: '#5e548e' }}
+               activeColor="#ffffff"
+               inactiveColor="#ffffff"
+               
+               >
+       <Stack.Screen name="Home" component={Home} options={{ tabBarIcon:() => ( <AntDesign name="home" size={24} color="white" />)}}/>
+
+        <Stack.Screen
+        options={{ tabBarIcon:() => (<MaterialIcons name="multitrack-audio" size={24} color="white" />)}}
+          name="Audio"
+          component={Audio}
+        />
+        <Stack.Screen
+        options={{ tabBarIcon:() => ( <Feather name="video" size={24} color="white" /> )}}
+          name="Video"
+          component={Video}
+ 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 import tw from 'twrnc';
 
 const Hello = () => {
   return (
-    <View style={tw`flex justify-center items-center bg-blue-700`}>
-      <Text style={tw`text-5xl`}>Hi</Text>
+    <View style={tw`flex justify-center items-center min-h-full bg-[#231942]`}>
+      <Image source={require('../img/disc.png')} />
+      <Text style={tw`text-7xl font-extrabold text-purple-400 pt-2 `}>
+        Vidius
+      </Text>
+
+      <Text style={tw`text-xl text-white font-bold pt-2 text-center `}>
+        The next gen media player made with react-native and expo
+      </Text>
+      <View style={tw`flex flex-row pt-1`}>
+        <Button
+          title="Video"
+          color="#ffffff"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          title="Audio"
+          color="#ffffff"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
     </View>
   );
 };
